0a57afe4d4c940bbd4fe861a7d74c8c015ffff88
dagruck <34676046+dagruck@users.noreply.github.com>
2017-12-19 16:43:06 +0300

    Последний апдейт с переменными

---
+++ Sandbox/out/production/classes/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class MyFirstProgramm {
+   public static void main (String[] args) {
+      System.out.println("Hello ww");
+      double l = 8.0;
+      int s = 4;
+      String Me = "test";
+      System.out.println( (Me) + s + l);
+
+      System.out.println("S quad"+5+5);
+   }
+}
